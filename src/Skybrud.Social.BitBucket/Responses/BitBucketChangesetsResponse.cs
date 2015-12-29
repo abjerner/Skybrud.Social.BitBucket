7619b38bdb575ceace6bd1b5deb86adc858a7401
Anders Bjerner <abjerner@skybrud.dk>
2015-12-29 16:27:57 +0100

    Initial commit (code copied from the main Skybrud.Social repository)

---
+++ src/Skybrud.Social.BitBucket/Responses/BitBucketChangesetsResponse.cs
@@ -0,0 +1,34 @@
+using Skybrud.Social.BitBucket.Objects;
+using Skybrud.Social.Json;
+
+namespace Skybrud.Social.BitBucket.Responses {
+    
+    public class BitBucketChangesetsResponse {
+
+        #region Properties
+
+        public BitBucketChangeset[] Changesets { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        internal BitBucketChangesetsResponse() {
+            // Hide default constructor
+        }
+
+        #endregion
+
+        #region Methods
+        
+        public static BitBucketChangesetsResponse Parse(JsonObject obj) {
+            return new BitBucketChangesetsResponse {
+                Changesets = obj.GetArray("changesets", BitBucketChangeset.Parse)
+            };
+        }
+
+        #endregion
+
+    }
+
+}
