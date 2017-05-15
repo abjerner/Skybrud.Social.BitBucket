1133a00be95a4724124aeb596c9aeb557d1691e6
Anders Bjerner <abjerner@skybrud.dk>
2017-05-15 18:32:35 +0200

    First (beta) release :D

--- src/Skybrud.Social.BitBucket/Properties/AssemblyInfo.cs
+++ src/Skybrud.Social.BitBucket/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Skybrud.Social.BitBucket")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Authentication and service implementation for the BitBucket API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.Social")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyInformationalVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta001")]
 [assembly: AssemblyMetadata("StartDate", "2015-12-29")]
